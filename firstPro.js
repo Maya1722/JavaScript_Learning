227c9d40eb04c62417087861f2b7f426aa63859b
maya shingne <mayamhaske1722@gmail.com>
2022-10-04 14:21:07 +0530

    Create firstPro.js
    first program execution in js

---
+++ firstPro.js
@@ -0,0 +1,8 @@
+console.log("Maya Anil Shingne");
+console.log("My Hobbies - 1.listening music 2.travel 3.eat");
+console.info("My personal strength- hardwork, selfmotivated, manage time ");
+console.warn("My Weakness - Emotional ,sleep late night");
+console.log(30+50);
+console.log(49*479);
+
+
