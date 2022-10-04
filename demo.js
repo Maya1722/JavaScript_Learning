e680979db4d6fc607c8da9592b078fe03c95d296
maya shingne <mayamhaske1722@gmail.com>
2022-10-04 08:36:59 +0530

    add demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,6 @@
+//this is my program
 console.log("hello");
+console.log("my name is maya");
+console.info("this is info");//info means information
+console.warn("i am not that bad guy");//warn means warning
+console.error("oops try later");// 
